5b7cd314f5cf151a45822cab74ce4b46d65ad88b
blackprince001 <appiahboaduprince@gmail.com>
2025-02-24 15:18:59 +0000

    new role tags for users

--- internal/domain/schemas/user.go
+++ internal/domain/schemas/user.go
@@ -8,14 +8,14 @@ type UserUpdate struct {
 type Credentials struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
-	Role     string `json:"user_role" binding:"required,oneof=student parent teacher admin"`
+	Role     string `json:"user_role" binding:"required,oneof=common admin"`
 }
 
 type CreateAccountDetails struct {
 	Username string `json:"username" binding:"required,min=3,max=50"`
 	Password string `json:"password" binding:"required,min=8"`
 	Email    string `json:"email" binding:"required,email"`
-	UserRole string `json:"user_role" binding:"required,oneof=student parent teacher admin"`
+	UserRole string `json:"user_role" binding:"required,oneof=common admin"`
 }
 
 type ForgotPasswordRequest struct {
@@ -29,4 +29,13 @@ type ResetPasswordRequest struct {
 
 type VerificationDetails struct {
 	Email string `json:"email" binding:"required,email"`
+}
+
+type UserResponse struct {
+	ID            string `gorm:"type:uuid;primary_key;" json:"id"`
+	Username      string `gorm:"size:50;unique;not null" json:"username"`
+	Password      string `gorm:"size:255;not null" json:"password"`
+	Email         string `gorm:"size:100;unique;not null" json:"email"`
+	UserRole      string `gorm:"type:varchar(20);default:'common'" json:"user_role"`
+	EmailVerified bool   `gorm:"default:false" json:"is_verified"`
 }
