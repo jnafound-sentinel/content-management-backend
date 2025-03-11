d117d75cc204c68333b62420e192763793d37e49
blackprince001 <appiahboaduprince@gmail.com>
2025-03-11 03:04:33 +0000

    remove status from donation field

--- internal/domain/models/payments/donations.go
+++ internal/domain/models/payments/donations.go
@@ -6,16 +6,12 @@ import (
 )
 
 type Donation struct {
-	ID uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-
-	TagName string `gorm:"size:50" json:"tag_name"`
-	Purpose string `gorm:"size:100;default:'foundation'" json:"purpose"`
-
-	Amount      int64  `gorm:"type:bigint" json:"amount"`
-	Status      string `gorm:"size:100" json:"status"`
-	Description string `gorm:"type:text" json:"description"`
-
-	Payments []Payment `gorm:"foreignKey:DonationID" json:"payments"`
+	ID          uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	TagName     string    `gorm:"size:50" json:"tag_name"`
+	Purpose     string    `gorm:"size:100;default:'foundation'" json:"purpose"`
+	Amount      int64     `gorm:"type:bigint" json:"amount"`
+	Description string    `gorm:"type:text" json:"description"`
+	Payments    []Payment `gorm:"foreignKey:DonationID" json:"payments"`
 }
 
 func (b *Donation) BeforeCreate(tx *gorm.DB) (err error) {
